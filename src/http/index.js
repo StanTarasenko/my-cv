75e0d40a7d2304f7abe0a685aa34a17fc5a598ff
Stan Tarasenko <stan.tarasenko1991@gmail.com>
2023-10-18 21:16:43 +0300

    deconstract app

--- src/http/index.js
+++ src/http/index.js
@@ -2,8 +2,10 @@
 import axios from 'axios';
 import { REACT_APP_API_URL } from '../constants/baseApi';
 
+const baseURL = process.env.NODE_ENV === 'production' ? '' : REACT_APP_API_URL;
+
 const $host = axios.create({
-  baseURL: REACT_APP_API_URL
+  baseURL,
 })
 
 export {
